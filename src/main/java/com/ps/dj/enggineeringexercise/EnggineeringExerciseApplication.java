e592e6fef74a288a68db90188f54753ec0ab1743
Deepak Jain <djain11@sapient.com>
2018-08-24 13:02:52 +0530

    First Commit

---
+++ src/main/java/com/ps/dj/enggineeringexercise/EnggineeringExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.ps.dj.enggineeringexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnggineeringExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnggineeringExerciseApplication.class, args);
+	}
+}
